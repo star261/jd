31a295bcea8cfaba969c7b59e2ce1aabbe64fe62
star <869728223@qq.com>
2021-10-31 10:09:47 +0800

    update

--- scripts/jd_hotNeight.js
+++ scripts/jd_hotNeight.js
@@ -22,6 +22,10 @@ let shareList = [];
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
         return;
     }
+    if(Date.now() > 1635696000000){
+        console.log(`活动已结束`);
+        return ;
+    }
     for (let i = 0; i < cookiesArr.length; i++) {
         $.index = i + 1;
         $.cookie = cookiesArr[i];
